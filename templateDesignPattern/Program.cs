d8ec5d752a9d041099012ef910324a7a66a6ad0f
aysud <aysud@DESKTOP-3BJ5QS9>
2022-01-16 17:32:13 +0300

    Add project files.

---
+++ templateDesignPattern/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace templateDesignPattern
+{
+    enum OdemeTipi // Değişkenlerin alabileceği değerlerin sabit  olduğu durumlarda programı daha okunabilir hale getirmek için kullanılır. 
+    { 
+    Pesin,Taksit
+    }
+    abstract class Alisveris
+    {
+        protected string ProductName;
+        protected OdemeTipi odemeTipi;
+        void Baslat()
+        {
+            Console.WriteLine("alışveriş başladı");
+        }
+        void Bitir()
+        {
+            Console.WriteLine($"alışveriş bitti.{ProductName} {odemeTipi} yöntemiyle alınmıştır.");
+        
+        }
+
+        abstract public void Urun();
+        abstract public void OdemeSekli();
+        public void TemplateMethod()
+        {
+            Baslat();
+            Urun();
+            OdemeSekli();
+            Bitir();
+        }
+
+    
+    
+    }
+
+    class Televizyon :Alisveris // concrete class
+    {
+        public override void OdemeSekli()
+        {
+            odemeTipi = OdemeTipi.Pesin;
+        }
+        public override void Urun()
+        {
+            ProductName = "televizyon";
+                
+        }
+    }
+    class Buzdolabi : Alisveris //concrete class
+    {
+        public override void OdemeSekli()
+        {
+            odemeTipi = OdemeTipi.Taksit;
+        }
+        public override void Urun()
+        {
+            ProductName = "buzdolabı";
+
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alisveris alisveris1 = new Televizyon();
+            alisveris1.TemplateMethod();
+            Console.WriteLine("***********");
+            Alisveris alisveris2 = new Buzdolabi();
+            alisveris2.TemplateMethod();
+
+            Console.Read();
+        }
+    }
+}
